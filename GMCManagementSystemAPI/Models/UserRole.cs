2f2399ceb475e4855fc54c63c7acb7a2de282b73
avnishsingh7 <avnish7.singh@hotmail.com>
2021-07-10 18:21:47 +0530

    user role changes .

--- GMCManagementSystemAPI/Models/UserRole.cs
+++ GMCManagementSystemAPI/Models/UserRole.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GMCManagementSystemAPI.ViewModel
+namespace GMCManagementSystemAPI.Models
 {
     /// <summary>
     /// 
