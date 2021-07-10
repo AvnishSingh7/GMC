3d046a8532f1ff32c2c8e8b63de622b9ea89d803
Vijay Jha <vijay.j@avowstech1.onmicrosoft.com>
2021-07-10 18:08:24 +0530

    First Commit

---
+++ GMCManagementSystemAPI/ViewModel/UserRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GMCManagementSystemAPI.ViewModel
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserRole
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
